7419e6e071b6d468eff981dcd9f2b940363e5582
Viper3489753489 <32537257+Viper3489753489@users.noreply.github.com>
2019-03-13 17:34:51 +0200

    outdated change

--- ServiceTesting/server.js
+++ ServiceTesting/server.js
@@ -3,6 +3,9 @@ var app = express();
 var server = require("http").createServer(app);
 var os = require("os")
 var bodyParser = require('body-parser');
+var Console = require("node-console-input");
+
+const terminal = require("./terminal.js");
 
 
 var PORT = 5555;
@@ -12,17 +15,21 @@ PORT = Console.getConsoleInput("Please select a port: \n",true);//Force input
 console.log("Waiting for Incoming connections on "+HostAddress+":" + PORT);
 console.log("--------------------------------------------------");
 server.listen(PORT);
+var numRequestsDone = 0;
 app.use(express.static(__dirname+ "js"));
 app.use(bodyParser.json()); 
 app.post("/",function(req,res){
-	console.log("Receiving data");
+	// console.log("Receiving data");
 
-
-	console.log(req.body)
+	console.log("Receiving Request: "+ (++numRequestsDone));
+	//console.log(req.body)
 
 	res.json({
 		"status":"200",
 		"message":"Successly read your data"
 	})
-	res.end()
+	res.end();
 });
+
+
+terminal.AllowInput();
