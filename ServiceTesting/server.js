e63e1c7f294c8b0aeb0f39784b186ee3628ab2d5
Viper3489753489 <32537257+Viper3489753489@users.noreply.github.com>
2019-03-13 13:02:05 +0200

    integrated email with api service

--- ServiceTesting/server.js
+++ ServiceTesting/server.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
-var os = require("os")
+var os = require("os");
+var Console = require("node-console-input");
 var bodyParser = require('body-parser');
+
+
+var Mailer = require("./SendMail.js");
 
 
 var PORT = 5555;
@@ -14,15 +18,25 @@ console.log("--------------------------------------------------");
 server.listen(PORT);
 app.use(express.static(__dirname+ "js"));
 app.use(bodyParser.json()); 
-app.post("/",function(req,res){
-	console.log("Receiving data");
+app.post("/",async function(req,res){
+	// console.log("Receiving data");
+	try{
 
+		// console.log(req.body)
+		var data = req.body;
+		// console.log("Sending mail to clientID - "+data.ClientID);
+		data.ClientID = "u16009917@tuks.co.za";
+		var mailFeedback = await Mailer.sendMail(data.ClientID,data.Type,data.Content);
+		console.log(mailFeedback);
+		mailFeedback = JSON.parse(mailFeedback);
+		/*If success log*/
 
-	console.log(req.body)
+		/*If fail log*/
 
-	res.json({
-		"status":"200",
-		"message":"Successly read your data"
-	})
-	res.end()
+		/*Send feedback to the person who requested our service*/
+		res.json(mailFeedback);		
+		res.end();
+	}catch(error){
+		console.log(error);
+	}
 });
