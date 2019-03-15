1aa381778cd6b2d42765452a7f41c4fdc3244c7a
Viper3489753489 <32537257+Viper3489753489@users.noreply.github.com>
2019-03-15 07:37:44 +0200

    added logging functions

--- ServiceTesting/terminal.js
+++ ServiceTesting/terminal.js
@@ -1,83 +1,77 @@
-/**
-* @param io: Socket 
-* @param connection: A 1D array containing on the sockets connected to the server
-*/
-exports.AllowInput= function(io,connections){
+/*This version should support input from terminal*/
+// and it accomplish this by including the logs js
+// const sqlite3 = require('sqlite3').verbose();
+// let database = new sqlite3.Database('db/logs.db', (err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     console.log('Connected to the in-memory SQlite database.');
+//   });
+
+var logs = require("./logs")	
+
+exports.ConsoleInput = function(){
 	//Allow the terminal to multitask by creating another process decicated to listening for commands
 	var stdin = process.openStdin();
-	if(connections == null)
-		console.log("Conntections are null")
+
 	stdin.addListener("data", function(d) {
 	  	var commands = d.toString().trim();
 	  	commands = commands.split(" ");
-	  	if(commands.length == 1)
-	  		output= command(commands[0],"",io,connections)
+	  	if(commands.length == 1 && commands[0] != 'exit')
+	  	{
+	  		client_id = commands[0];
+	  		timestamp = null;
+	  		notificationType = null;
+	  		restrict = "all";
+	  		output= logs.pullLogs(client_id, timestamp, notificationType, restrict);
+	  	}
+	  	else if(commands[0] == 'exit')
+	  	{
+	  		console.log("============ Stopping Server ============");
+			process.exit();
+	  	}
 	  	else
-	  		output = command(commands[0],commands[1],io,connections);
+	  	{
+			client_id = commands[0];
+	  		timestamp = commands[1];
+	  		notificationType = commands[2];
+	  		restrict = commands[3];
+	  		output = pullLogs(client_id, timestamp, notificationType, restrict);
+	  	}
 	  	if(output == false){
 	  		console.log(false);
 	  	}
+
+
 	});
-
-//Specify what commands can be used
-function command(cmd, id="",io,connections){
-	var socket = io.sockets;
-	switch(cmd){
-		case "1":
-		case "kill":{
-			//close the id
-			if(id==""){
-				console.log("Incorrect use of the command 'kill', please specify an ID aswell");
-				break;
-			}
-
-			try{
-				if(socket.sockets[connections[id-1].id]){
-					socket.sockets[connections[id-1].id].disconnect();
-					connections.remove(id-1);
-					return;
-				}
-			}
-			catch(error){
-				console.log("Please Select A valid id between 0 AND "+connections.length-1);
-			}
-			break;
-		}
-		case "3":
-		case "restart":{
-			console.log("Restarting All connections");
-			io.close();
-			break;
-		}
-		case "4":
-		case "exit":{
-			console.log("============ Closing All connections ============");
-			io.close();
-			stdin.removeAllListeners('data');
-			console.log("============ Stopping Server ============");
-			process.exit();
-			break;
-		}
-		case "2":
-		case "list":{
-			console.log("============ Connections ============");
-			for (var i = 0; i < connections.length; i++) {
-				var user = connections[i].username
-				if(user == undefined)
-					user = "Unknown";
-				console.log("Connection "+(i+1)+"'s IP: "+connections[i].id +" ,Since: "+user);
-			}
-			console.log("");
-			break;
-		}
-		default:{
-			console.log("============ Commands ============");
-			console.log("1) kill <id>");
-			console.log("2) list");
-			console.log("3) restart");
-			console.log("4) exit");
-			console.log("");
+/*
+	function pullLogs(client_id, timestamp, notificationType, restrict) 
+	{
+		if(client_id)
+		{
+			let respond = SearchAll(client_id);
+			return respond;
 		}
 	}
+	function SearchAll(log_client_id) 
+	{
+		let sql= `SELECT id,log_client_id, log_type, log_content, log_timestamp FROM logs WHERE log_client_id=?`;
+    	database.get(sql, [log_client_id], (err, row) => {
+
+		  if (err) {
+		      	return console.error(err.message);
+		      }
+		      if(row)
+		      {
+		      	var jsonResp = '{"log_id":"'+ row.id+'", "log_timestamp":"'+ row.log_timestamp +'", "log_client_id":"'+row.log_client_id + '", "log_type":"'+row.log_type +'","log_content":"'+row.log_content +'"}';
+		      	return console.log(jsonResp);
+		      }
+		      else
+
+		      return console.log(`No logs are found`);
+		  });
+	}
+
+
 }
-}
+*/
